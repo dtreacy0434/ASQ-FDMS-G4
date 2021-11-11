b2db1ed46b45b1e9d383218fca3f2d2d522809d5
dtreacy0434 <92539995+dtreacy0434@users.noreply.github.com>
2021-11-11 00:51:33 -0500

    Add files via upload

---
+++ TelemData.cs
@@ -0,0 +1,23 @@
+﻿namespace ASQ_DB
+{
+    class TelemData
+    {
+        public string AircraftTailNumber { get; set;  }
+
+        public string StorageTime { get; set; }
+
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public float Z { get; set; }
+
+        public float Weight { get; set; }
+
+        public float Altitude { get; set; }
+
+        public float Pitch { get; set; }
+
+        public float Bank { get; set; }
+    }
+}
